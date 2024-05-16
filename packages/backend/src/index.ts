4d9c98c1315df030e4e12f965acc4874023f91e9
Kurt-Urban <kurturban00@gmail.com>
2024-05-16 12:29:37 -0400

    feat: api setup complete

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 const port = 3001;
 
 app.get("/", (req: Request, res: Response) => {
--- packages/lambda/main.py
+++ packages/lambda/main.py
@@ -1,1 +1,5 @@
-print("Hello World!")
+import json
+
+
+def lambda_handler(event, context):
+    return {"statusCode": 200, "body": json.dumps("Hello from Lambda!")}
