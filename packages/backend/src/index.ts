57c4b5420022a56e0fb956182b933f1ba0bc9c5a
Kurt-Urban <kurturban00@gmail.com>
2024-05-18 13:43:13 -0400

    feat: s3 upload functional

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,12 +1,89 @@
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+
 import express, { Request, Response } from "express";
 import cors from "cors";
+import bodyParser from "body-parser";
+import AWS from "aws-sdk";
+import multer from "multer";
+
+AWS.config = new AWS.Config({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+});
 
 const app = express();
 app.use(cors());
 const port = 3001;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, World!");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const upload = multer({ storage: multer.memoryStorage() });
+
+app.post("/", upload.single("file"), (req: Request, res: Response) => {
+  const file = req?.file;
+  if (file) {
+    const params = {
+      Bucket: "scrubber-user-uploads",
+      Key: file.originalname,
+    };
+
+    s3.headObject(params, (err, data) => {
+      if (err && err.code === "NotFound") {
+        // File does not exist in S3, proceed with upload
+        const uploadParams = {
+          Bucket: "scrubber-user-uploads",
+          Key: file.originalname,
+          Body: file.buffer,
+          Metadata: {
+            ...JSON.parse(req.body.params),
+          },
+        };
+
+        Object.keys(uploadParams.Metadata).forEach((key) => {
+          uploadParams.Metadata[key] = String(uploadParams.Metadata[key]);
+        });
+
+        s3.upload(
+          uploadParams,
+          (err: Error, _: AWS.S3.ManagedUpload.SendData) => {
+            if (err) {
+              console.error("Error uploading file to S3:", err);
+              res.status(500).send("Error uploading file to S3");
+            } else {
+              res.status(200).send("File uploaded to S3");
+            }
+          }
+        );
+      } else if (data) {
+        // File already exists in S3, return error response
+        res.status(400).send("File already exists in S3");
+      } else {
+        // Error occurred while checking S3, return error response
+        console.error("Error checking S3:", err);
+        res.status(500).send("Error checking S3");
+      }
+    });
+  } else {
+    res.status(400).send("No file found in the request");
+  }
+});
+
+const s3 = new AWS.S3();
+
+app.get("/buckets", (req: Request, res: Response) => {
+  s3.listBuckets((err, data) => {
+    if (err) {
+      console.error("Error listing buckets:", err);
+      res.status(500).send("Error listing buckets");
+    } else {
+      const bucketNames = data.Buckets?.map((bucket) => bucket.Name) || [];
+      res.status(200).json(bucketNames);
+    }
+  });
 });
 
 app.listen(port, () => {
--- packages/frontend/app/page.tsx
+++ packages/frontend/app/page.tsx
@@ -2,18 +2,51 @@
 import React from "react";
 import axios from "axios";
 
+type cleaningParams = {
+  dropNa: boolean;
+  dropDuplicates: boolean;
+  dropColumns: string[];
+  fillNa: boolean;
+  fillNaValue?: "median" | "mean" | "mode" | "backwards" | number | string;
+};
+
 export default function Home() {
+  const [file, setFile] = React.useState<File | null>(null);
+
   const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
     event.preventDefault();
-    const file = event.dataTransfer.files[0];
-    // Handle the dropped file
-    console.log(file);
+    const droppedFile = event.dataTransfer.files[0];
+    setFile(droppedFile);
+  };
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      setFile(event.target.files[0]);
+    }
   };
 
   const handleTestClick = async () => {
+    if (!file) return;
+
+    const params: cleaningParams = {
+      dropNa: true,
+      dropDuplicates: true,
+      dropColumns: ["column1", "column2"],
+      fillNa: true,
+      fillNaValue: "mean",
+    };
+
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("params", JSON.stringify(params));
+
     try {
-      const response = await axios.get("http://localhost:3001/");
-      console.log(response.data);
+      const res = await axios.post("http://localhost:3001/", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      console.log(res);
     } catch (error) {
       if (axios.isAxiosError(error)) {
         console.error(error.message);
@@ -34,12 +67,17 @@ export default function Home() {
       <div className="container mx-auto">
         <div className="flex justify-center mt-10">
           <div
-            className="w-1/2 hover:cursor-pointer"
+            className="w-1/2"
             onDrop={handleDrop}
             onDragOver={(event) => event.preventDefault()}
           >
-            <input type="file" className="w-full" placeholder="Upload a file" />
-            <div className="mt-4 border-2 border-dashed border-gray-400 rounded-lg p-4">
+            <input
+              type="file"
+              className="w-full"
+              placeholder="Upload a file"
+              onChange={handleFileChange}
+            />
+            <div className="mt-4 bg-gray-300 border-2 border-dashed border-gray-400 rounded-lg p-4">
               <p className="text-gray-500 text-center">
                 Drag and drop files here
               </p>
