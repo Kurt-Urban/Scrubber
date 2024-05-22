e2a518454d27ae08c6f2afbaebb436f4d40ce2a3
KofiShang <kfs5346@psu.edu>
2024-05-22 12:18:51 -0400

    feat: created lambda Preprocessing

--- packages/lambda/main.py
+++ packages/lambda/main.py
@@ -1,5 +1,69 @@
 import json
-
+import boto3
+import pandas as pd
+import io
 
 def lambda_handler(event, context):
-    return {"statusCode": 200, "body": json.dumps("Hello from Lambda!")}
+    bucket_name = event['bucket']
+    file_key = event['file_key']
+    cleaning_options = event['cleaning_options']
+
+    # Read File
+    s3 = boto3.client('s3')
+    obj = s3.get_object(Bucket=bucket_name, Key=file_key)
+    data = pd.read_csv(io.BytesIO(obj['Body'].read()))
+
+    # Preprocess Data
+    processed_data, report = preprocess_data(data, cleaning_options)
+
+    # Save Processed File
+    output_buffer = io.BytesIO()
+    processed_data.to_csv(output_buffer, index=False)
+    output_buffer.seek(0)
+    s3.put_object(Bucket=bucket_name, Key='processed_' + file_key, Body=output_buffer)
+
+    # Save Report
+    report_key = 'report_' + file_key.replace('.csv', '.txt')
+    s3.put_object(Bucket=bucket_name, Key=report_key, Body=report)
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps({
+            'processed_file_key': 'processed_' + file_key,
+            'report_file_key': report_key
+        })
+    }
+
+def preprocess_data(data, options):
+    report = []
+    if 'remove_duplicates' in options:
+        data.drop_duplicates(inplace=True)
+        report.append("Removed duplicates")
+
+    if 'correct_data_formats' in options:
+        data = correct_data_formats(data)
+        report.append("Corrected data formats")
+
+    if 'fill_missing_values' in options:
+        data = fill_missing_values(data)
+        report.append("Filled missing values")
+
+    return data, "\n".join(report)
+
+def correct_data_formats(data):
+    for column in data.select_dtypes(include=['object']).columns:
+        try:
+            data[column] = pd.to_datetime(data[column])
+        except (ValueError, TypeError):
+            pass
+
+    return data
+
+def fill_missing_values(data):
+    for column in data.columns:
+        if data[column].dtype == 'object':
+            data[column].fillna('missing', inplace=True)
+        else:
+            data[column].fillna(data[column].mean(), inplace=True)
+
+    return data
