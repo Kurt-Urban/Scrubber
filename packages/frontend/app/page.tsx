ec8fafd2390655b69d32d49c8406cb55bd34978c
Kurt-Urban <kurturban00@gmail.com>
2024-05-28 14:18:09 -0400

    feat: adjusted btn interactions

--- packages/frontend/app/page.tsx
+++ packages/frontend/app/page.tsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Formik, FormikProps, Form } from "formik";
-import { Checkbox, Dropdown, Radio } from "@/components";
+import { Checkbox, Dropdown, Radio, Spinner } from "@/components";
 import * as yup from "yup";
 import classNames from "classnames";
 import { MdCheckBox, MdOutlineDeleteOutline } from "react-icons/md";
@@ -15,7 +15,12 @@ export type CleaningParams = {
   fillCustomNaValue?: string;
 };
 
-const defaultParams: CleaningParams = {
+interface FormValues extends CleaningParams {
+  confirmSend: boolean;
+}
+
+const defaultValues: FormValues = {
+  confirmSend: false,
   dropDuplicates: true,
   dropColumns: [],
   fillNa: "none",
@@ -27,7 +32,9 @@ const validationSchema = yup.object().shape({
   fillCustomNaValue: yup
     .string()
     .when("fillNaValue", ([fillNaValue], schema) =>
-      fillNaValue === "custom" ? schema.required("Required Field.") : schema
+      fillNaValue === "custom"
+        ? schema.required("Required Field.").nullable()
+        : schema
     ),
 });
 
@@ -35,6 +42,7 @@ export default function Home() {
   const [file, setFile] = useState<File | null>(null);
   const [columns, setColumns] = useState<string[]>([]);
   const [fileError, setFileError] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   useEffect(() => {
     if (!file) return;
@@ -42,14 +50,17 @@ export default function Home() {
     (async () => {
       try {
         const fileContents = await file.text();
+        if (!file.name.endsWith(".csv")) {
+          setFile(null);
+          setFileError(true);
+          return;
+        }
+        setFileError(false);
+        setIsLoading(false);
         const lines = fileContents.split("\n");
         if (lines.length > 0) {
           const columns = lines[0].split(",");
           setColumns(columns);
-        }
-        if (!file.name.endsWith(".csv")) {
-          setFile(null);
-          setFileError(true);
         }
       } catch (error) {
         console.error(error);
@@ -57,25 +68,35 @@ export default function Home() {
     })();
   }, [file]);
 
-  const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
+  const handleFileEvent = (
+    event:
+      | React.DragEvent<HTMLDivElement>
+      | React.ChangeEvent<HTMLInputElement>,
+    resetForm: () => void
+  ) => {
     event.preventDefault();
-    const droppedFile = event.dataTransfer.files[0];
-    setFile(droppedFile);
-  };
+    let file;
 
-  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files) {
-      setFile(event.target.files[0]);
+    if ("dataTransfer" in event) {
+      file = event.dataTransfer.files[0];
+    } else if (event.target.files) {
+      file = event.target.files[0];
     }
+
+    if (file) {
+      setFile(file);
+    }
+    resetForm();
   };
 
-  const handleSubmit = async (params: CleaningParams) => {
+  const handleSubmit = async (params: FormValues) => {
     if (!file) return;
     const formData = new FormData();
     formData.append("file", file);
     formData.append("params", JSON.stringify(params));
 
     try {
+      setIsLoading(true);
       const res = await axios.post("http://localhost:3001/", formData, {
         headers: {
           "Content-Type": "multipart/form-data",
@@ -100,45 +121,61 @@ export default function Home() {
         <h1 className="text-4xl font-thin">Scrubber CSV Cleaner</h1>
       </div>
       <Formik
-        initialValues={defaultParams}
+        initialValues={defaultValues}
         onSubmit={handleSubmit}
         validationSchema={validationSchema}
       >
-        {({ values, setFieldValue }: FormikProps<CleaningParams>) => {
+        {({
+          values,
+          setFieldValue,
+          resetForm,
+          submitCount,
+        }: FormikProps<FormValues>) => {
           return (
             <Form className="container mx-auto">
               <div className="flex justify-center mt-10">
                 <div
                   className="mx-auto w-1/2 relative border-2 border-dashed border-blue-gray-400 bg-blue-gray-200 rounded-lg p-4 flex justify-center transition-opacity duration-150 hover:opacity-80"
-                  onDrop={handleDrop}
+                  onDrop={(e) => handleFileEvent(e, resetForm)}
                   onDragOver={(e) => e.preventDefault()}
                 >
                   <input
                     type="file"
                     className="absolute top-0 w-full h-full opacity-0 cursor-pointer"
-                    onChange={handleFileChange}
+                    onChange={(e) => handleFileEvent(e, resetForm)}
                   />
                   <span className="text-blue-gray-700">
                     {file?.name || "Drop a file here or click to upload"}
                   </span>
                 </div>
               </div>
-
+              {file ? (
+                <div className="text-center">
+                  <small
+                    onClick={() => setFile(null)}
+                    className={classNames(
+                      "text-blue-gray-700 text-xs cursor-pointer transition-all duration-200 hover:underline hover:text-blue-900",
+                      { "opacity-0 pointer-events-none": submitCount }
+                    )}
+                  >
+                    Clear File
+                  </small>
+                </div>
+              ) : null}
+              {fileError ? (
+                <div className="text-red-500 text-center mt-2">
+                  Please upload a .csv file.
+                </div>
+              ) : null}
               <div
                 className={classNames(
-                  "container  w-1/2 mx-auto mt-8 transition-all duration-500",
+                  "container w-1/2 mx-auto mt-1 transition-all duration-500",
                   {
                     "opacity-0 pointer-events-none": !file,
                   }
                 )}
               >
-                <button
-                  type="submit"
-                  className="w-full bg-blue-600 hover:bg-blue-800 text-white p-2 rounded-md relative transition-opacity duration-500"
-                >
-                  Clean .CSV
-                </button>
-                <div className="mt-4">
+                <div>
                   <strong className="text-lg text-blue-gray-900">
                     Table Columns to Process:
                   </strong>
@@ -206,6 +243,47 @@ export default function Home() {
                     />
                   </div>
                   <Dropdown />
+                </div>
+                <div className="mt-3">
+                  <div className="relative h-10">
+                    <button
+                      type="submit"
+                      disabled={isLoading || submitCount > 0}
+                      className={classNames(
+                        "absolute w-full bg-green-600 text-white p-2 rounded-md transition-all duration-500 ease-in-out",
+                        {
+                          "opacity-50 cursor-not-allowed": isLoading,
+                          "hover:bg-green-800": !isLoading,
+                          "opacity-0 pointer-events-none": !values.confirmSend,
+                          "opacity-100": values.confirmSend,
+                        }
+                      )}
+                    >
+                      {isLoading ? (
+                        <div className="flex justify-center">
+                          <Spinner />
+                        </div>
+                      ) : (
+                        "Confirm"
+                      )}
+                    </button>
+                    <button
+                      type="button"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        setFieldValue("confirmSend", true);
+                      }}
+                      className={classNames(
+                        "absolute w-full bg-blue-600 hover:bg-blue-800 text-white p-2 rounded-md transition-all duration-500 ease-in-out",
+                        {
+                          "opacity-0 pointer-events-none": values.confirmSend,
+                          "opacity-100": !values.confirmSend,
+                        }
+                      )}
+                    >
+                      Clean .CSV
+                    </button>
+                  </div>
                 </div>
               </div>
             </Form>
--- packages/frontend/components/index.ts
+++ packages/frontend/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Checkbox } from "./Checkbox";
 export { default as Radio } from "./Radio";
 export { default as Dropdown } from "./Dropdown";
+export { default as Spinner } from "./Spinner";
